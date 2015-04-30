fcd7f2dcc4501699f4939abcdff7cb026723bdd2
jl0ve <jlove934@uw.edu>
2015-04-30 16:05:55 -0700

    Added search method in admin

--- Urban_Parks/src/model/Admin.java
+++ Urban_Parks/src/model/Admin.java
@@ -1,12 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Admin {
 	
+	//First Name Variable
 	private String myFirst;
+	//Last Name Variable
 	private String myLast;
+	//Email Variable
 	private String myEmail;
+	//Password Variable
 	private String myPassword;
 	
+	/**
+	 * Constructor
+	 */
 	public Admin() {
 		myFirst = "John";
 		myLast = "Doe";
@@ -14,6 +26,13 @@ public class Admin {
 		myPassword = "password";
 	}
 
+	/**
+	 * Constructor
+	 * @param theFirst, String
+	 * @param theLast, String
+	 * @param theEmail, String
+	 * @param thePassword, String
+	 */
 	public Admin(String theFirst, String theLast, String theEmail, String thePassword) {
 		myFirst = theFirst;
 		myLast = theLast;
@@ -21,6 +40,12 @@ public class Admin {
 		myPassword = thePassword;
 	}
 	
+	/**
+	 * This method logs admin in with given email and password
+	 * @param email, string
+	 * @param password, string
+	 * @return string if they were loged in or not
+	 */
 	public String logIn(String email, String password) {
 		if (!email.equals(myEmail))
 			return "Your email or password does not match our database";
@@ -29,4 +54,28 @@ public class Admin {
 		return "Welcome! " + myFirst + " " + myLast;
 	}
 	
+	/**
+	 * Searches through the database for the input lastname and return
+	 * a arraylist of userid if found or return null if nothing was found
+	 * @param theLastName, the last name of the Volunteer
+	 * @return ArrayList<Integer>, List of user id with the given last name
+	 */
+	public ArrayList<Integer> getVolunteer(String theLastName) {
+		ArrayList<Integer> foundVolunteer = null;
+		
+		UserList list = new UserList();
+		HashMap<Integer, Object[]> map = list.getMap();
+		
+		java.util.Iterator<Entry<Integer, Object[]>> itr = map.entrySet().iterator();
+		while(itr.hasNext()) {
+			Map.Entry<Integer, Object[]> pair = (Map.Entry<Integer, Object[]>)itr.next();
+			//This checks value[1] to equal the given last name and checks to see if its a volunteer
+			if (pair.getValue()[1].equals(theLastName) && pair.getValue()[2] == (Integer)0) {
+				foundVolunteer.add(pair.getKey());
+			}
+			itr.remove();
+		}
+		
+		return foundVolunteer;
+	}
 }
