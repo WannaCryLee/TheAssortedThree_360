51319efd0f04009e58d72119773b4e3fc51efa93
Jordan Love <jlove934@uw.edu>
2015-05-01 19:58:42 -0700

    Added TestSuite

---
+++ Urban_Parks/testsrc/model/AllTests.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AdminTest.class, CerealTest.class, VolunteerTest.class })
+public class AllTests {
+
+}
