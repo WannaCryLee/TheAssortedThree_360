96d46c29e43230e2cd8bf27056ab13e80c7db022
arielm.nu <arielm.nu@gmail.com>
2015-05-02 22:51:05 -0700

    Still working on Testing Volunteer.

--- Urban_Parks/src/model/Volunteer.java
+++ Urban_Parks/src/model/Volunteer.java
@@ -45,7 +45,7 @@ public class Volunteer implements Serializable {
 		myPassword = thePassword;
 		myJobSignedUp = new ArrayList<Job>();
 	}
-	
+
 	/**
 	 * logIn provides the volunteer with the ability to enter into the system.
 	 * @param theEmail the email of the signing in volunteer
@@ -62,16 +62,51 @@ public class Volunteer implements Serializable {
 		return "Welcome! " + myFirst + " " + myLast;
 	}
 	/**
-	 * addJob places the Job the volunteer signed up for in a list.
+	 * addJob places the Job the volunteer signed up for in a list, if they do not already have a job that day
+	 * and they it's not already in their list.
 	 * @param theJob, the position the volunteer wishes to sign up for
 	 */
 	public String addJob(Job theJob){
-		//if date in theJob != date in any job in list then
-		myJobSignedUp.add(theJob);
-		//return "Success!! You are signed up to volunteer for " + theJob.title + " on " + theJob.date + "."
-		//else 
-		return "You are already volunteering for a job on this day.";
+		Job jobArray = new Job();
+		boolean volunteered = false;
+//		String success = "Success!! You are signed up to volunteer for " 
+//				+ theJob.getTitle() + " on "+ theJob.getDate() + ".";
+		if(myJobSignedUp.size() != 0){	
+			System.out.println("Inside if {size > 0}");
+			for(int i = 0; i < myJobSignedUp.size(); i++){
+				jobArray = myJobSignedUp.get(i);
+				System.out.println(jobArray.getTitle());
+				
+				//if date in theJob != date in any job in list then
+				if(theJob.getDate() == jobArray.getDate()){
+					volunteered = true;
+					//myJobSignedUp.add(theJob);
+					//return success;
+				} 
+//				else {
+//					System.out.println("You are already volunteering for a job on this day!");
+//					return "You are already volunteering for a job on this day!";
+//				}
+			}
+		} 
+//		else {
+//			myJobSignedUp.add(theJob);
+//			//return success;
+//		}
+		System.out.println(volunteered);
+		if(volunteered){
+			System.out.println("here");
+			return "You are already volunteering for a job on this day!";
+		} else {
+			System.out.println("here");
+			return "Success!! You are signed up to volunteer for " + theJob.getTitle() + " on "; //+ theJob.date + ".";
+		}
 		
+		//return success;
+		
+		//else 
+		//return "You are already volunteering for a job on this day.";
+
 	}
 
 	/**
@@ -85,7 +120,7 @@ public class Volunteer implements Serializable {
 		myFirst = theFirst;
 	}
 
-	
+
 	/**
 	 * Getters and Setter for Last Name
 	 */
@@ -97,7 +132,7 @@ public class Volunteer implements Serializable {
 		myLast = theLast;
 	}
 
-	
+
 	/**
 	 * Getters and Setter for email
 	 */
@@ -109,7 +144,7 @@ public class Volunteer implements Serializable {
 		myEmail = theEmail;
 	}
 
-	
+
 	/**
 	 * Getters and Setter for password
 	 */
@@ -121,7 +156,7 @@ public class Volunteer implements Serializable {
 		myPassword = thePassword;
 	}
 
-	
+
 	/**
 	 * Getters and Setter for Job signed up
 	 */
@@ -132,7 +167,7 @@ public class Volunteer implements Serializable {
 	public void setMyJobSignedUp(List<Job> theJobSignedUp) {
 		myJobSignedUp = theJobSignedUp;
 	}
-	
-	
-	
+
+
+
 }
--- Urban_Parks/testsrc/model/VolunteerTest.java
+++ Urban_Parks/testsrc/model/VolunteerTest.java
@@ -12,29 +12,25 @@ import org.junit.Test;
  *
  */
 public class VolunteerTest {
-
+			
 	private Volunteer volunteerDefault = new Volunteer();
 	private Volunteer volunteerGivenData = new Volunteer("Lysia", "Valu", "jedimaster@gmail.com", "sparkle");
+	private Job jobData1 = new Job("Lessons", "Huntamer Park", "Lacey Dr.", "Swimming in the air.", 5, "August 1");
+	private Job jobData2 = new Job("Training", "Long Lake", "Canyon Rd.", "Lifeguard Training", 5, "June 2");
 	/**
 	 * Test method for {@link model.Volunteer#Volunteer(int, java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
 	 */
 	@Test
-	public void testVolunteerGivenData() {
+	public void testLogIn() {
 		//Testing constructor with no parameters to log in
 		assertEquals(volunteerDefault.logIn("doe@gmail.com", "password"), "Your email or password does not match our database");
 		assertEquals(volunteerDefault.logIn("janedoe@gmail.com", "pass"), "Your email or password does not match our database");
 		assertEquals(volunteerDefault.logIn("janedoe@gmail.com", "password"), "Welcome! Jane Doe");
 		
 		//Testing constructor with parameters to log in
+		assertEquals(volunteerGivenData.logIn("jedi@gmail.com", "sparkle"), "Your email or password does not match our database");
+		assertEquals(volunteerGivenData.logIn("jedimaster@gmail.com", "spark"), "Your email or password does not match our database");
 		assertEquals(volunteerGivenData.logIn("jedimaster@gmail.com", "sparkle"), "Welcome! Lysia Valu");
-	}
-
-	/**
-	 * Test method for {@link model.Volunteer#logIn(java.lang.String, java.lang.String)}.
-	 */
-	@Test
-	public void testLogIn() {
-		fail("Not yet implemented");
 	}
 
 	/**
@@ -42,7 +38,9 @@ public class VolunteerTest {
 	 */
 	@Test
 	public void testAddJob() {
-		fail("Not yet implemented");
+		assertEquals(volunteerDefault.addJob(jobData1), "Success!! You are signed up to volunteer for " + jobData1.getTitle() + " on ");
+		//assertEquals(volunteerDefault.addJob(jobData1), "You are already volunteering for a job on this day!");
+		//assertEquals(volunteerDefault.addJob(jobData2), "Success!! You are signed up to volunteer for " + jobData2.getTitle() + " on ");
 	}
 
 }
